fdb58dbd693d44924d8d397d314fa27c165c5020
Simon Oulevay <simon.oulevay@gmail.com>
2016-11-24 23:16:39 +0100

    Remember current exercise.

--- public/javascripts/exercises.js
+++ public/javascripts/exercises.js
@@ -6,7 +6,7 @@
     template: `
       <uib-accordion>
         <div class='panel-default' ng-repeat='exercise in ctrl.exercises' uib-accordion-group heading='{{ exercise.title }} (Exercise {{ exercise.number }})' is-open='ctrl.accordion["ex" + exercise.number]'>
-          <exercise-view exercise='exercise' is-open='ctrl.accordion["ex" + exercise.number]'></exercise-view>
+          <exercise-view exercise='exercise' is-open='ctrl.accordion["ex" + exercise.number]' on-open='ctrl.open(exercise)'></exercise-view>
         </div>
       </uib-accordion>
     `,
@@ -15,15 +15,30 @@
     bindings: {}
   });
 
-  app.controller('ExercisesListCtrl', function(ExercisesService) {
+  app.controller('ExercisesListCtrl', function(ExercisesService, $location, $rootScope) {
 
     var ctrl = this;
 
     ctrl.exercises = ExercisesService.exercises;
 
-    ctrl.accordion = {
-      ex1: true
+    var exerciseNumber = parseInt($location.search().exercise, 10);
+    if (isNaN(exerciseNumber) || !_.isNumber(exerciseNumber) || exerciseNumber < 1 || exerciseNumber > ExercisesService.exercises.length) {
+      exerciseNumber = 1;
+    }
+
+    ctrl.accordion = {};
+    ctrl.accordion['ex' + exerciseNumber] = true;
+
+    ctrl.open = function(exercise) {
+      $location.search('exercise', exercise.number);
     };
+
+    $rootScope.$on('$locationChangeSuccess', function(event, newState, oldState) {
+      var newExerciseNumber = $location.search().exercise;
+      if (!ctrl.accordion['ex' + newExerciseNumber]) {
+        ctrl.accordion['ex' + newExerciseNumber] = true;
+      }
+    });
   });
 
   app.component('exerciseView', {
@@ -32,14 +47,22 @@
     controllerAs: 'ctrl',
     bindings: {
       exercise: '<',
-      isOpen: '<'
+      isOpen: '<',
+      onOpen: '&'
     }
   });
 
   app.controller('ExerciseViewCtrl', function($compile, $element, $scope) {
 
-    var exercise = this.exercise,
+    var ctrl = this,
+        exercise = ctrl.exercise,
         template = '<' + exercise.component + ' exercise="ctrl.exercise" is-open="ctrl.isOpen"></' + exercise.component + '>';
+
+    ctrl.$onChanges = function(changes) {
+      if (changes.isOpen && changes.isOpen.currentValue) {
+        ctrl.onOpen();
+      }
+    }
 
     $element.append($compile(template)($scope));
   });
